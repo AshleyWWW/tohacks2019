491e208f31e4c8e73c1203df14b6e1ea2dbb5be8
AshleyWWW <ashley.x.y.w@gmail.com>
2019-06-23 01:41:59 -0400

    add student status match

--- matcher.js
+++ matcher.js
@@ -12,21 +12,24 @@ dataTable = [
         "url": "https://static1.squarespace.com/static/56d1fe3ed210b8b23f16d059/t/5aaff3042b6a28d1b413f7ca/1521480453390/Incoming+Refugee+Sponsorship+Bursary+%282%29.pdf",
         "ageMin": 17,
         "ageMax": 30,
-        "provinces": ["ANY"]
+        "provinces": ["ANY"],
+        "studentStatus": "Y"
     },
     {
         "name": "Resettlement Assistance Program (RAP)  - All Provinces, excluding Quebec",
         "url": "http://www.rstp.ca/wp-content/uploads/2019/03/Start-up.pdf",
         "ageMin": 0,
         "ageMax": 100,
-        "provinces": ["Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", "Saskatchewan"]
+        "provinces": ["Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", "Saskatchewan"],
+        "studentStatus": "N"
     }, 
     {
         "name" : "Alberta Resettlement Assistance Program (RAP) Rate",
         "url" : "http://www.rstp.ca/wp-content/uploads/2018/08/Alberta-RAP-rates.pdf",
         "ageMin" : 0,
         "ageMax" : 100,
-        "provinces" : ["Alberta"]
+        "provinces" : ["Alberta"],
+        "studentStatus": "N"
     }
 ];
 
@@ -38,9 +41,14 @@ app.get('/', (req, res) => {
         function locationMatch(places, destination) {
             return destination ? (places.includes('ANY') || places.includes(destination)) : blankMeansYes;
         }
+        function studentStatusMatch(requirement, status) {
+            return status ? (requirement == 'ANY' || requirement == status) : blankMeansYes;
+        }
+        // etc etc
 
         return (ageMatch(element.ageMin, element.ageMax, profile.age) 
-            && locationMatch(element.provinces, profile.destination));
+            && locationMatch(element.provinces, profile.destination)
+            && studentStatusMatch(element.studentStatus, profile.studentStatus));
     }
 
     results = [];
