608c0c3861eff0ae99d9e03ff06171e073bc8e02
AshleyWWW <ashley.x.y.w@gmail.com>
2019-06-23 02:16:23 -0400

    add comments

--- matcher.js
+++ matcher.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// if the user doesn't specify anything, does that mean they match
+// yes they match = true
 const blankMeansYes = true; 
 
 // Hard-coded values for testing
@@ -33,32 +35,40 @@ dataTable = [
     }
 ];
 
+// what to do when the GET request comes in
 app.get('/', (req, res) => {
+    // does this person's profile match this element (entry of the data table)?
     function computeMatch(element, profile) {
+        // does their age match
         function ageMatch(min, max, age) {
-            return age ? (age >= min && age <= max) : blankMeansYes;
+            return age ? (age >= min && age <= max) : blankMeansYes; // ? : is called the ternary operator btw
         }
+        // does their location match
         function locationMatch(places, destination) {
             return destination ? (places.includes('ANY') || places.includes(destination)) : blankMeansYes;
         }
+        // does their student status/not match
         function studentStatusMatch(requirement, status) {
             return status ? (requirement == 'ANY' || requirement == status) : blankMeansYes;
         }
-        // etc etc
+        // etc etc, can someone else do this
 
+        //return whether or not the criteria all matches
         return (ageMatch(element.ageMin, element.ageMax, profile.age) 
             && locationMatch(element.provinces, profile.destination)
             && studentStatusMatch(element.studentStatus, profile.studentStatus));
     }
 
+    // the bursaries the user qualifies for 
     results = [];
+    // for each bursary in the table
     dataTable.forEach((element) => {
         if (computeMatch(element, req.query)) {
-            results.push(element);
+            results.push(element); // the person is suited to this grant/bursary/thing
         }
     });
 
-    res.send(results); // send the good entries
+    res.send(results); // send back the matches
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
