699c0e1ee486f9e0e10365997a5b4d6e65404439
AshleyWWW <ashley.x.y.w@gmail.com>
2019-06-23 08:17:49 -0400

    prep a query string

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+const headers = ['name', 'url', 'provinces', 'ageMin', 'ageMax', 'language', 'studentStatus', 'school', 'gpa', 'maritalStat', 'dependants', 'ethnicity', 'yearsInCanada'] // renamed headers, etc
+const headerNames = ['NAME OF BURSARY', 'LINK', 'ENTRY PROVINCE', 'AGE MIN', 'AGE MAX', 'ENGLISH/FRENCH COMPETENT (oral and written)', 'STUDENT-BASED(Y/N)', 'SCHOOL OF STUDY (NONE if not student, ANY for any school)', 'GRADE AVERAGE (NONE, if none needed)', 'MARRIAGE STATUS (Married, Single, ANY)', 'NUMBER OF DEPENDANTS (ANY for acceptance regardless)', 'ETHNICITY (ANY for open acceptance)', 'Minimum Number of Years in Canada'];
+
 document.getElementById("submitBtn").onclick = function () {
-    fetch("http://localhost:3000/?age=20", {
+    function assembleQueryString() {
+        var query = "?" + 
+                    "originCountry=" + document.getElementById("originCountry").innerHTML +
+                    "&yearsInCanada=" + document.getElementById("yearsCDN").innerHTML + 
+                    "&destination=" + document.getElementById("hostProv").innerHTML + 
+                    "&age=" + document.getElementById("age").innerHTML +
+                    "&maritalStatus=" + document.getElementById("userProf").innerHTML +
+                    "&school=" + document.getElementById("school").innerHTML +
+                    "&dependants=" + document.getElementById("userDependant").innerHTML;
+    }
+    
+    fetch("http://localhost:3000/" + query, {
         mode: 'cors'
     }).then(function (data) {
         return data.json();
