6292a9b4685312c24bc53b20b54f3e6dfcbca40b
AshleyWWW <ashley.x.y.w@gmail.com>
2019-06-22 20:51:14 -0400

    provide more detail about match success or fail

--- Matcher.java
+++ Matcher.java
@@ -1,13 +1,20 @@
 public class Matcher {
+    
+    private class Match {
+        public boolean pass; 
+        public boolean strict; // if true, failing this is a hard no
+        public String message; 
+    }
+
     public static double computeScore() {
         return 0.0; 
     }
 
-    public static boolean ageMatch() {
-        return false; 
+    public static Match ageMatch() {
+        return new Match(); 
     }
 
-    public static boolean provinceMatch() {
-        return false; 
+    public static Match provinceMatch() {
+        return new Match(); 
     }
 }
