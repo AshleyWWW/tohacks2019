95004042c85e266e04ec9b7ead8ce26405e9a024
AshleyWWW <ashley.x.y.w@gmail.com>
2019-06-23 06:13:28 -0400

    clean up code

--- matcher.js
+++ matcher.js
@@ -64,20 +64,15 @@ app.get('/', (req, res) => {
     function computeMatch(element, profile) {
         // does their age match
         function ageMatch(min, max, age) {
-            console.log(element.name + " ageMatch " + min + " " + max + " " + age + " " + (age ? (age >= min && age <= max) : blankMeansYes));
-            console.log((age) + (age >= min) + (age <= max) + blankMeansYes);
             return age ? (age >= min && age <= max) : blankMeansYes; // ? : is called the ternary operator btw
         }
         // does their location match
         function locationMatch(places, destination) {
-            console.log(element.name + " loc " + (destination ? (places.includes('ANY') || places.includes(destination)) : blankMeansYes));
-            //return destination ? (`${places}`.includes('ANY') || `${places}`.includes(`${destination}`)) : blankMeansYes;
-			return destination ? (places.includes('ANY') || places.includes(destination)) : blankMeansYes;
+            return destination ? (places.includes('ANY') || places.includes(destination)) : blankMeansYes;
 			
         }
         // does their student status/not match
         function studentStatusMatch(requirement, status) {
-            console.log(element.name + " stu " + (status ? (requirement == 'ANY' || requirement == status) : blankMeansYes));
             return status ? (requirement == 'ANY' || requirement == status) : blankMeansYes;
         }
         // etc etc, can someone else do this
@@ -92,44 +87,11 @@ app.get('/', (req, res) => {
     results = [];
     // for each bursary in the table
     dataTable.forEach((element) => {
-		console.log("\n" + element.name)
-		//alert(element.name)
-        if (computeMatch(element, req.query)) {
-            console.log(element.name)
-			results.push(element); // the person is suited to this grant/bursary/thing
+		if (computeMatch(element, req.query)) {
+            results.push(element); // the person is suited to this grant/bursary/thing
         }
     });
-
     res.send(results); // send back the matches
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-/* // Hard-coded values for testing
-// TODO replace with code that reads from the "database"
-dataTable = [
-    {
-        "name": "Incoming Refugee Sponsorship Bursary (IRSB) - ON",
-        "url": "https://static1.squarespace.com/static/56d1fe3ed210b8b23f16d059/t/5aaff3042b6a28d1b413f7ca/1521480453390/Incoming+Refugee+Sponsorship+Bursary+%282%29.pdf",
-        "ageMin": 17,
-        "ageMax": 30,
-        "provinces": ["ANY"],
-        "studentStatus": "Y"
-    },
-    {
-        "name": "Resettlement Assistance Program (RAP)  - All Provinces, excluding Quebec",
-        "url": "http://www.rstp.ca/wp-content/uploads/2019/03/Start-up.pdf",
-        "ageMin": 0,
-        "ageMax": 100,
-        "provinces": ["Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", "Saskatchewan"],
-        "studentStatus": "N"
-    }, 
-    {
-        "name" : "Alberta Resettlement Assistance Program (RAP) Rate",
-        "url" : "http://www.rstp.ca/wp-content/uploads/2018/08/Alberta-RAP-rates.pdf",
-        "ageMin" : 0,
-        "ageMax" : 100,
-        "provinces" : ["Alberta"],
-        "studentStatus": "N"
-    }
-]; */
+app.listen(port, () => console.log(`App listening on port ${port}!`));
