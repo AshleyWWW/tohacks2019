4c4bdbb97f27546d969fa534b00f12500db9c283
AshleyWWW <ashley.x.y.w@gmail.com>
2019-06-23 05:30:12 -0400

    make fetch request trigger on button push

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-fetch("http://localhost:3000/?age=20", {
-    mode: 'cors'
-}).then(function (data) {
-    return data.json();
-}).then(function (body) {
-    headers = ['name', 'url', 'provinces', 'ageMin', 'ageMax', 'language', 'studentStatus', 'school', 'gpa', 'maritalStat', 'dependants', 'ethnicity', 'yearsInCanada'] // renamed headers, etc
-    var table = "<table border='1'><tr>";    
-    headers.forEach(header => {
-        table += "<td>" + header + "</td>";
-    });
-    body.forEach(element => {
-        table += "<tr>" 
+document.getElementById("submitBtn").onclick = function () {
+    fetch("http://localhost:3000/?age=20", {
+        mode: 'cors'
+    }).then(function (data) {
+        return data.json();
+    }).then(function (body) {
+        headers = ['name', 'url', 'provinces', 'ageMin', 'ageMax', 'language', 'studentStatus', 'school', 'gpa', 'maritalStat', 'dependants', 'ethnicity', 'yearsInCanada'] // renamed headers, etc
+        var table = "<table border='1'><tr>";
         headers.forEach(header => {
-            table += "<td>" + element[header] + "</td>";
+            table += "<td>" + header + "</td>";
         });
-        table += "</tr>";
+        body.forEach(element => {
+            table += "<tr>"
+            headers.forEach(header => {
+                table += "<td>" + element[header] + "</td>";
+            });
+            table += "</tr>";
+        });
+        table += "</table>"
+        document.getElementById("output").innerHTML = table;
     });
-    table += "</table>"
-    document.getElementById("output").innerHTML = table;
-})
+}; 
