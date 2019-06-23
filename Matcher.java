431fc8be84df4d2497fd869a3efcaa764099e4f2
AshleyWWW <ashley.x.y.w@gmail.com>
2019-06-22 20:48:37 -0400

    create a basic matcher class and some basic placeholders

---
+++ Matcher.java
@@ -0,0 +1,13 @@
+public class Matcher {
+    public static double computeScore() {
+        return 0.0; 
+    }
+
+    public static boolean ageMatch() {
+        return false; 
+    }
+
+    public static boolean provinceMatch() {
+        return false; 
+    }
+}
