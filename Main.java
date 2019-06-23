a3a266a99dd5295910484c056dc138191a041c69
pyritefish <eleanor.jiang@gmail.com>
2019-06-22 20:44:14 -0400

    some more testing and memery

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
+/*
+    An epic meme of a hackathon
+*/
+
 public class Main {
     
-    public static void main() {
+    public static void main(String[] args) {
         // placeholder
         System.out.println("end my misery");
+        //int q = dice(4);
+        System.out.println(dice(4)); // should be 2
     }
+
+    // Testing, to make sure I haven't forgetten how to call classes
+    public static int dice(int n){
+        return n/2;
+
+    }
+
+
 }
