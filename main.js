62dea8cf9cb55d492e1965b49aa1ab62cbcfe535
AshleyWWW <ashley.x.y.w@gmail.com>
2019-06-23 04:40:05 -0400

    process fetched data into basic HTML table

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-fetch("http://localhost:3000/", {
+fetch("http://localhost:3000/?age=20", {
     mode: 'cors'
 }).then(function (data) {
-    return data.text();
+    return data.json();
 }).then(function (body) {
-    console.log(body);
-    document.getElementById("output").innerHTML = body;
+    headers = ['name', 'url', 'provinces', 'ageMin', 'ageMax', 'language', 'studentStatus', 'school', 'gpa', 'maritalStat', 'dependants', 'ethnicity', 'yearsInCanada'] // renamed headers, etc
+    var table = "<table border='1'><tr>";    
+    headers.forEach(header => {
+        table += "<td>" + header + "</td>";
+    });
+    body.forEach(element => {
+        table += "<tr>" 
+        headers.forEach(header => {
+            table += "<td>" + element[header] + "</td>";
+        });
+        table += "</tr>";
+    });
+    table += "</table>"
+    document.getElementById("output").innerHTML = table;
 })
